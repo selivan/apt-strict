c99c622acfcafd5485cf952c1fc5de5ac81fbdf9
Ilya Rusalowski <rivik@yandex-team.ru>
2015-09-07 22:47:25 +0300

    fixed git url

--- apt-strict.rb
+++ apt-strict.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class AptStrict < Formula
   homepage "https://github.com/selivan/apt-strict"
-  url "git@github.com:rivik/apt-strict.git", :using => :git
+  url "git@github.com:selivan/apt-strict.git", :using => :git
   version "0.5-10"
 
   depends_on "ansible"
